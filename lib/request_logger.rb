b0a538953de8ec2011b8e304eba127d766581825
Vitaly Kushner <vitaly@astrails.com>
2009-09-07 06:55:15 +0300

    small refactoring

--- lib/request_logger.rb
+++ lib/request_logger.rb
@@ -3,8 +3,7 @@ module RequestLogger
   def log_request
     options = self.class.read_inheritable_attribute(:request_logger_options)
 
-    request_info = {
-      :session_id => request.session_options[:id],
+    rec = RequestLog.new :session_id => request.session_options[:id],
       :uri        => request.try(:url),
       :remote_ip  => request.try(:remote_ip),
       :user_agent => request.try(:user_agent),
@@ -14,13 +13,12 @@ module RequestLogger
       :referer    => request.referer,
       :params     => respond_to?(:filter_parameters) ? filter_parameters(params).inspect : params.inspect,
       :status     => response.headers["Status"]
-    }
 
     if options[:request_info]
-      request_info = self.send(options[:request_info], request_info)
+      self.send(options[:request_info], rec)
     end
 
-    RequestLog.create request_info
+    rec.save!
   rescue => e
     logger.error "Failed to log request: #{e}"
     logger.debug e.backtrace * "\n"
